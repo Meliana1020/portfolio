f75e194ab08a48127b10f6f6cdeb66e2bf655c1d
Meliana1020 <melianaa1020@gmail.com>
2024-11-18 11:39:53 +0700

    edit position sidebar when md

--- src/Components/Elements/SidebarMenu/sidebarMenu.jsx
+++ src/Components/Elements/SidebarMenu/sidebarMenu.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-scroll";
 
 const SidebarMenu = () => {
   return (
-    <div className="fixed right-7 left-7 lg:right-0 sm:right-7 bottom-6 lg:bottom-20 sm:bottom-24 justify-between py-6 bg-zinc-700 flex flex-row lg:flex-col md:flex-row md:bottom-4 md:right-10 md:w-[40%] md:h-12 lg:w-[4%] lg:h-[65%] sm:flex-col items-center sm:justify-between sm:gap-3 md:gap-4 lg:gap-6 gap-2 lg:m-7 border rounded-xl z-50 sm:h-96 sm:w-14 w-80 h-10 sm:pt-4 sm:pb-4">
+    <div className="fixed right-5 lg:right-0 sm:right-7 md:justify-end bottom-6 lg:bottom-20 sm:bottom-24 justify-between py-6 bg-zinc-700 flex flex-row lg:flex-col md:flex-row md:bottom-4 md:w-[40%] md:h-12 lg:w-[4%] lg:h-[65%] sm:flex-col items-center sm:justify-between sm:gap-3 md:gap-4 lg:gap-6 gap-2 lg:m-7 border rounded-xl z-50 sm:h-96 sm:w-14 w-80 h-10 sm:pt-4 sm:pb-4">
       <Link
         activeClass="active bg-orange-400 rounded-full scale-50"
         to="home"
         spy={true}
         smooth={true}
         offset={-80}
-        duration={50}
+        duration={25}
         className="p-4 sm:py-5"
       >
         <img
@@ -26,7 +26,7 @@ const SidebarMenu = () => {
         spy={true}
         smooth={true}
         offset={-80}
-        duration={50}
+        duration={25}
         className="p-4 sm:py-5"
       >
         <img
@@ -42,7 +42,7 @@ const SidebarMenu = () => {
         spy={true}
         smooth={true}
         offset={-70}
-        duration={50}
+        duration={25}
         className="p-4 sm:py-5"
       >
         <img
@@ -58,7 +58,7 @@ const SidebarMenu = () => {
         spy={true}
         smooth={true}
         offset={-80}
-        duration={50}
+        duration={25}
         className="p-4 sm:py-5"
       >
         <img
@@ -74,7 +74,7 @@ const SidebarMenu = () => {
         spy={true}
         smooth={true}
         offset={-80}
-        duration={50}
+        duration={25}
         className="p-4 sm:py-5"
       >
         <img
