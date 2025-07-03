b3014c46b53b7b51293751e980e8c671a4a45993
Meliana1020 <melianaa1020@gmail.com>
2025-07-04 00:24:59 +0700

    update endpoint railway

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://portfolio-meliana.vercel.app"
+    "https:/portfolio-production-c2ad.up.railway.app"
   ],
   methods: ["POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"]
